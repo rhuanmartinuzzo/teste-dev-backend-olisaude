cf30b81010b0f90c44b5a4dcc0f2a0de19e2695e
Rhuan <rhuanmcr@gmail.com>
2022-10-05 20:35:04 -0300

    Começo da API

--- olisaude/src/main/java/br/com/olisaude/model/User.java
+++ olisaude/src/main/java/br/com/olisaude/model/User.java
@@ -2,8 +2,6 @@ package br.com.olisaude.model;
 
 
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,17 +10,11 @@ import java.util.*;
 
 @Entity
 @Table(name = "users")
-public class User implements UserDetails, Serializable {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "user_name", unique = true)
-    private String user_name;
-
-    @Column(name = "password")
-    private String password;
 
     @Column(name = "name")
     private String name;
@@ -39,71 +31,7 @@ public class User implements UserDetails, Serializable {
     @Column(name = "updated_at")
     private Date updated_at;
 
-    @Column(name = "account_non_expired")
-    private Boolean accountNonExpired;
-
-    @Column(name = "account_non_locked")
-    private Boolean accountNonLocked;
-
-    @Column(name = "credentials_non_expired")
-    private Boolean credentialsNonExpired;
-
-    @Column(name = "enabled")
-    private Boolean enabled;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name ="user_permission", joinColumns = {@JoinColumn (name = "id_user")},
-        inverseJoinColumns = {@JoinColumn(name = "id_permission")}
-    )
-
-    private List<Permission> permissions;
-
     public User(){}
-
-    public List<String> getRoles(){
-        List<String> roles = new ArrayList<>();
-
-        for (Permission permission: permissions){
-            roles.add(permission.getDescription());
-        }
-        return roles;
-    }
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.permissions;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.user_name;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return this.accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return this.credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
-    }
 
     public Long getId() {
         return id;
@@ -111,18 +39,6 @@ public class User implements UserDetails, Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getName() {
@@ -165,84 +81,27 @@ public class User implements UserDetails, Serializable {
         this.updated_at = updated_at;
     }
 
-    public Boolean getAccountNonExpired() {
-        return accountNonExpired;
-    }
-
-    public void setAccountNonExpired(Boolean accountNonExpired) {
-        this.accountNonExpired = accountNonExpired;
-    }
-
-    public Boolean getAccountNonLocked() {
-        return accountNonLocked;
-    }
-
-    public void setAccountNonLocked(Boolean accountNonLocked) {
-        this.accountNonLocked = accountNonLocked;
-    }
-
-    public Boolean getCredentialsNonExpired() {
-        return credentialsNonExpired;
-    }
-
-    public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
-        this.credentialsNonExpired = credentialsNonExpired;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public List<Permission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(List<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof User user)) return false;
 
         if (!Objects.equals(id, user.id)) return false;
-        if (!Objects.equals(user_name, user.user_name)) return false;
-        if (!Objects.equals(password, user.password)) return false;
         if (!Objects.equals(name, user.name)) return false;
         if (!Objects.equals(birth_day, user.birth_day)) return false;
         if (!Objects.equals(gender, user.gender)) return false;
         if (!Objects.equals(created_at, user.created_at)) return false;
-        if (!Objects.equals(updated_at, user.updated_at)) return false;
-        if (!Objects.equals(accountNonExpired, user.accountNonExpired))
-            return false;
-        if (!Objects.equals(accountNonLocked, user.accountNonLocked))
-            return false;
-        if (!Objects.equals(credentialsNonExpired, user.credentialsNonExpired))
-            return false;
-        if (!Objects.equals(enabled, user.enabled)) return false;
-        return Objects.equals(permissions, user.permissions);
+        return Objects.equals(updated_at, user.updated_at);
     }
 
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (user_name != null ? user_name.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (birth_day != null ? birth_day.hashCode() : 0);
         result = 31 * result + (gender != null ? gender.hashCode() : 0);
         result = 31 * result + (created_at != null ? created_at.hashCode() : 0);
         result = 31 * result + (updated_at != null ? updated_at.hashCode() : 0);
-        result = 31 * result + (accountNonExpired != null ? accountNonExpired.hashCode() : 0);
-        result = 31 * result + (accountNonLocked != null ? accountNonLocked.hashCode() : 0);
-        result = 31 * result + (credentialsNonExpired != null ? credentialsNonExpired.hashCode() : 0);
-        result = 31 * result + (enabled != null ? enabled.hashCode() : 0);
-        result = 31 * result + (permissions != null ? permissions.hashCode() : 0);
         return result;
     }
 }
